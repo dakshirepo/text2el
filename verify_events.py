10c2b7f3a0be972ec0a88dfcf410839b6ec3641d
dakshirepo <104542083+dakshirepo@users.noreply.github.com>
2022-06-12 22:49:57 +1000

    Update verify_events.py

--- verify_events.py
+++ verify_events.py
@@ -1,13 +1,18 @@
 import re
 import pandas as pd
-#import sent2vec
+import sent2vec
 
-#from nltk import word_tokenize
+from nltk import word_tokenize
 
 from string import punctuation
-#from gensim.utils import simple_preprocess
-#from scipy.spatial import distance
-#from nltk.stem import WordNetLemmatizer
+from gensim.utils import simple_preprocess
+from scipy.spatial import distance
+from nltk.stem import WordNetLemmatizer
+
+import pandas as pd
+from pm4py.objects.log.util import dataframe_utils
+from pm4py.objects.conversion.log import converter as log_converter
+from pm4py.objects.log.exporter.xes import exporter as xes_exporter
 
 
 def time_analysis(df):
@@ -103,6 +108,36 @@ def get_matched_events(semantic_similariy_csv, extracted_matched_events_csv):
     matched_events = matched_events.loc[matched_events.groupby(["HADM_ID","extracted_Activity","Timestamp_x"])['Sim_Bio'].idxmax()].reset_index(drop=True)
     matched_events.to_csv(extracted_matched_events_csv)
 
+    
+def add_new_events(matched_events_csv, all_events_csv, event_log_csv, enhanced_log_csv):
+    d1= pd.read_csv(matched_events_csv)
+    d2= pd.read_csv(all_events_csv)
+    d3= pd.read_csv(event_log_csv)
+    d4= pd.read_csv(enhanced_log_csv)
+    
+    d2["Activity"]=d2["Activity"].str.lower()
+    d1["Activity"]=d1["Activity"].str.lower()
+    d2['Activity'] = d2['Activity'].str.strip()
+
+    d1_index = d1.set_index(['HADM_ID', 'Activity', 'Timestamp']).index
+    d2_index = d2.set_index(['HADM_ID', 'Activity','Timestamp']).index
+    mask = ~d2_index.isin(d1_index)
+    result = d2.loc[mask]
+    enh_event_log = pd.concat([d3, result])
+
+    enh_event_log.to_csv(enhanced_log_csv)
+    
+def convert_csv_xes(eventlog_csv, eventlog_xes):
+    log_csv = pd.read_csv(eventlog_csv)
+    log_csv.rename(columns = {'HADM_ID':'case:concept:name', 'Activity':'concept:name', 'Timestamp':'time:timestamp'}, inplace = True)
+
+    log_csv = dataframe_utils.convert_timestamp_columns_in_df(log_csv)
+    log_csv = log_csv.sort_values('time:timestamp')
+    event_log = log_converter.apply(log_csv)
+
+    xes_exporter.apply(event_log, eventlog_xes)
+    
+    
 if __name__ == '__main__':
     compare_events("all_events.csv", "event_log_evaluation.csv", "matched_events.csv", "time_matched_events.csv")
     #calcute_semantic_similarity(pre_trained_model_path, "time_matched_events.csv", "semantic_similariy.csv")
@@ -110,5 +145,7 @@ if __name__ == '__main__':
     # pre_trained_model_path = "/wiki_bigrams.bin"
     # pre_trained_model_path = "/twitter_bigrams.bin"
     get_matched_events("semantic_similariy.csv", "extracted_matched_events.csv")
+    add_new_events("matched_events.csv", "all_events.csv", "event_log.csv", "enhanced_log.csv")
+    convert_csv_xes('eventlog.csv', 'eventlog_xes')
 
 
